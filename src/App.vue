5af5a763b70dc793d90047473a6c5ad6fd35d144
chiefJurist <anthonynnanna004@gmail.com>
2023-08-19 01:38:51 +0100

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SignupForm />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    import SignupForm from "./components/SignupForm.vue"
+    export default {
+        name: 'App',
+        components: { SignupForm }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+    body{
+        margin: 0;
+        background: #eee;
+    }
 </style>
